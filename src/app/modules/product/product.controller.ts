55db5c2b2025c0e903577571551c11ababff190d
pantho0 <panthomashreky6@gmail.com>
2024-05-22 19:34:35 +0600

    search functionalites updated with fixing some errors

--- src/app/modules/product/product.controller.ts
+++ src/app/modules/product/product.controller.ts
@@ -17,35 +17,28 @@ const createProduct = async (req: Request, res: Response) => {
 
 const getAllProducts = async (req: Request, res: Response) => {
   try {
-    if (req.query) {
-      try {
-        const searchTerm = req.query.searchTerm as string;
-        const result = await productServices.productSearchIntoDB(
-          searchTerm as string,
-        );
-        if (result.length === 0) {
-          res.status(200).json({
-            success: true,
-            message: `No product found by your search term ${searchTerm}`,
-          });
-        } else {
-          res.status(200).json({
-            success: true,
-            message: `Product matching search term ${searchTerm} successfully`,
-            data: result,
-          });
-        }
-      } catch (error) {
-        console.log(error);
+    const searchTerm = req.query.searchTerm as string | null;
+    if (searchTerm) {
+      const result = await productServices.productSearchIntoDB(searchTerm);
+      if (result.length === 0) {
+        res.status(200).json({
+          success: true,
+          message: `No product found by your search term ${searchTerm}`,
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: `Product matching search term ${searchTerm} successfully`,
+          data: result,
+        });
       }
-    } else {
-      const result = await productServices.getAllProductsFromDB();
-      res.status(200).json({
-        success: true,
-        message: 'Product retrived successfully',
-        data: result,
-      });
     }
+    const result = await productServices.getAllProductsFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Product retrived successfully',
+      data: result,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -97,13 +90,10 @@ const deleteProduct = async (req: Request, res: Response) => {
   }
 };
 
-const productSearch = async (req: Request, res: Response) => {};
-
 export const productControllers = {
   createProduct,
   getAllProducts,
   getSingleProduct,
   updateProduct,
   deleteProduct,
-  productSearch,
 };
--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -5,7 +5,6 @@ const router = express.Router();
 router.post('', productControllers.createProduct);
 router.get('', productControllers.getAllProducts);
 router.get('/:productId', productControllers.getSingleProduct);
-// router.get('/?searchTerm', productControllers.productSearch);
 router.put('/:productId', productControllers.updateProduct);
 router.delete('/:productId', productControllers.deleteProduct);
 export const ProductRoutes = router;
